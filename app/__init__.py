637387447a0f689b194aa645cfffadfed789a07b
Will Nzeuton <wnzeuton50@stuy.edu>
2025-01-09 13:22:30 -0500

    headers

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,9 @@
+# Tim Ng, Daniel Park, Will Nzeuton, Yinwei Zhang
+# Team lobo2
+# SoftDev
+# p02
+# 2024-1-09
+
 from flask import Flask             
 from flask import render_template  
 
---
+++ app/db_routes.py
@@ -0,0 +1,5 @@
+# Tim Ng, Daniel Park, Will Nzeuton, Yinwei Zhang
+# Team lobo2
+# SoftDev
+# p02
+# 2024-1-09
