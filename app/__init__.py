451d2eb86008bf19b70c7d4e1876752527d42b0e
Will Nzeuton <wnzeuton50@stuy.edu>
2025-01-10 11:58:51 -0500

    cleanup init

--- app/__init__.py
+++ app/__init__.py
@@ -11,19 +11,10 @@ import os
 app = Flask(__name__)
 app.secret_key = os.urandom(32)
 
-BASE_DIR = os.path.abspath(os.path.dirname(__file__)) #link xase.db
-DB_PATH = os.path.join(BASE_DIR, 'xase.db')
-
-def get_db_connection():
-    conn = sqlite3.connect(DB_PATH)
-    conn.row_factory = sqlite3.Row
-    return conn
-
 @app.route("/")
 def home():
     return render_template('index.html')
 
-def
 if __name__ == "__main__":
     app.debug = True
     app.run()
