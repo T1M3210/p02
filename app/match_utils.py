e506159bbb10e251ac1878cc5850dff810418a0d
DP1117 <dpark50@stuy.edu>
2025-01-13 11:22:57 -0500

    match algo complete

--- app/match_utils.py
+++ app/match_utils.py
@@ -6,9 +6,16 @@ def pref_match(user1_id, user2_id):
     user2_prof = read_profile(user2_id)
     # Check requirements
     for data in user1_pref.keys():
-        prefs = user1_pref[data]["pref"]
+        # Preference is always a list
+        preference = user1_pref[data]["pref"]
+        required = user1_pref[data]["required"]
         profile = user2_prof[data]
-        if user1_pref[data]["required"]
+        if required and profile not in preference:
+            return -1
+        if profile in preference:
+            score += 1
+    return score
+
 
 def compatiblity_score(user1, user2):
     return pref_match(user1, user2) + pref_match(user2, user1)
